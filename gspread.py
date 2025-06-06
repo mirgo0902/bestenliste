28d734494bb92921dae9690d9747021561741502
mirgo0902 <mirgo0902@gmail.com>
2025-06-06 16:30:59 +0200

    Update gspread.py

--- gspread.py
+++ gspread.py
@@ -3,7 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Zugriff auf Google Sheet
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name("dein_google_api_schlüssel.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("bestenliste-462113-0ac9883ad436.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open("Bestenliste").sheet1
 
